2ae16af28a8423dd5408f5df7d20e3fef868893a
Gabriel <gabrielmnakashima2@gmail.com>
2026-01-10 10:19:23 -0300

    adicionando correção proposta da Alura

--- Dia 1/1dia.py
+++ Dia 1/1dia.py
@@ -3,4 +3,20 @@ import requests
 r = requests.get("https://last-airbender-api.fly.dev/api/v1/characters")
 arquivo = r.json()
 
+
 print(arquivo)
+
+#correcao da alura
+
+#import json
+#import requests
+
+#def personagens():
+
+#   api_url = f'https://last-airbender-api.fly.dev/api/v1/characters'
+
+#    response = requests.get(api_url)
+
+#    print(json.dumps(response.json(), indent=4))
+
+#personagens()
